3d057f5cb3c7a6ddf954d2f6f2b698a3560fae97
l123yuiop <l123yuiop@gmail.com>
2017-08-07 23:02:42 +0800

    add file
    add file

---
+++ 找詩歌譜/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient; //使用SQL
+namespace 找詩歌譜
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" + "AttachDbFilename=|DataDirectory|chord.mdf;" + "Integrated Security=True";
+            SqlConnection db = new SqlConnection(cn);
+            db.Open();
+            MessageBox.Show(db.Database, "資料庫");
+        }
+    }
+}
